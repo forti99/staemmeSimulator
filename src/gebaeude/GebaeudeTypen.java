530a58926de27a06e6f9f248c85a681ace29eac6
Michael Fortenbacher <forti99@web.de>
2022-09-12 16:52:27 +0200

    Ergaenzung der Id zu den Gebaeudetypen-Enums

--- src/gebaeude/GebaeudeTypen.java
+++ src/gebaeude/GebaeudeTypen.java
@@ -1,20 +1,26 @@
 package gebaeude;
 
 public enum GebaeudeTypen {
-    ADELSHOF,
-    BAUERNHOF,
-    EISENMINE,
-    HAUPTGEBAEUDE,
-    HOLZFAELLER,
-    KASERNE,
-    LEHMGRUBE,
-    MARKTPLATZ,
-    SCHMIEDE,
-    SPEICHER,
-    STALL,
-    WALL,
-    WERKSTATT,
-    STATUE,
-    VERSAMMLUNGSPLATZ,
-    VERSTECK
+    HAUPTGEBAEUDE(1), 
+    KASERNE(2),
+    STALL(3),
+    WERKSTATT(4),
+    ADELSHOF(5),
+    SCHMIEDE(6),
+    VERSAMMLUNGSPLATZ(7),
+    STATUE(8),
+    MARKTPLATZ(9),
+    HOLZFAELLER(10),
+    LEHMGRUBE(11),
+    EISENMINE(12),
+    BAUERNHOF(13),
+    SPEICHER(14),
+    VERSTECK(15),
+    WALL(16);
+        
+    public final int id;
+    
+    private GebaeudeTypen(int id){
+        this.id = id;
+    }
 }
