97134cc3bb747e52779c626039f108b95688efbb
Michael Fortenbacher <forti99@web.de>
2022-09-12 17:00:01 +0200

    Ergaenzung der maximalen Gebaeudestufen bei den GebaeudeTypen-Enums

--- src/gebaeude/GebaeudeTypen.java
+++ src/gebaeude/GebaeudeTypen.java
@@ -1,26 +1,37 @@
 package gebaeude;
 
 public enum GebaeudeTypen {
-    HAUPTGEBAEUDE(1), 
-    KASERNE(2),
-    STALL(3),
-    WERKSTATT(4),
-    ADELSHOF(5),
-    SCHMIEDE(6),
-    VERSAMMLUNGSPLATZ(7),
-    STATUE(8),
-    MARKTPLATZ(9),
-    HOLZFAELLER(10),
-    LEHMGRUBE(11),
-    EISENMINE(12),
-    BAUERNHOF(13),
-    SPEICHER(14),
-    VERSTECK(15),
-    WALL(16);
+    HAUPTGEBAEUDE(1, 30), 
+    KASERNE(2, 25),
+    STALL(3, 20),
+    WERKSTATT(4, 15),
+    ADELSHOF(5, 1),
+    SCHMIEDE(6, 20),
+    VERSAMMLUNGSPLATZ(7, 1),
+    STATUE(8, 1),
+    MARKTPLATZ(9, 25),
+    HOLZFAELLER(10, 30),
+    LEHMGRUBE(11, 30),
+    EISENMINE(12, 30),
+    BAUERNHOF(13, 30),
+    SPEICHER(14, 30),
+    VERSTECK(15, 10),
+    WALL(16, 20);
         
-    public final int id;
+    private final int id;
     
-    private GebaeudeTypen(int id){
+    private final int maxStufe;
+    
+    private GebaeudeTypen(int id, int maxStufe){
         this.id = id;
+        this.maxStufe = maStufe;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public int getMaxStufe(){
+       return maxStufe; 
     }
 }
