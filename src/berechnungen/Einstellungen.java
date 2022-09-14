0da45eeb1f7023252d7c31235c925de64279b562
Michael Fortenbacher <forti99@web.de>
2022-09-14 16:36:25 +0200

    Verlagerung der Belohnungseinstellung in Einstellung-Klasse

--- src/berechnungen/Einstellungen.java
+++ src/berechnungen/Einstellungen.java
@@ -7,7 +7,8 @@ public class Einstellungen {
     public static final int weltengeschwindigkeit = 2;
     public static final int minengeschwindigkeit = 2;
     public static final int[] produktionsraten = {10, 30, 35, 41, 47, 55, 64, 74, 86, 100, 117, 136, 158, 184, 214, 249, 289, 337, 391, 455, 530, 616, 717, 833, 969, 1127, 1311, 1525, 1774, 2063, 2400};
-
+    public static final boolean belohnungenAktiv = true;
+    
     //Berechnet die Belohnungen die fuer das erstmalige Bauen einer Gebaeudestufe erhalten werden (10% der Baukosten aber mindestens 150 Holz, 150 Lehm und 100 Eisen und maximal 2000 pro Rohstoff)
     public static Rohstoffe getBelohnung(GebaeudeTypen gebaeudeTyp, int stufe) {
         Rohstoffe baukosten = new GebaeudeDaten().getBaukosten(gebaeudeTyp, stufe);
