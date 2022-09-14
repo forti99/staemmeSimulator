dddd5ba795d751bb498a7cf88f8a7f325b40e277
Michael Fortenbacher <forti99@web.de>
2022-09-14 16:57:00 +0200

    Verschiebung der GebaeudeType-Ids auf einen Start ab 0

--- src/gebaeude/GebaeudeTypen.java
+++ src/gebaeude/GebaeudeTypen.java
@@ -1,22 +1,22 @@
 package gebaeude;
 
 public enum GebaeudeTypen {
-    HAUPTGEBAEUDE(1, 30),
-    KASERNE(2, 25),
-    STALL(3, 20),
-    WERKSTATT(4, 15),
-    ADELSHOF(5, 1),
-    SCHMIEDE(6, 20),
-    VERSAMMLUNGSPLATZ(7, 1),
-    STATUE(8, 1),
-    MARKTPLATZ(9, 25),
-    HOLZFAELLER(10, 30),
-    LEHMGRUBE(11, 30),
-    EISENMINE(12, 30),
-    BAUERNHOF(13, 30),
-    SPEICHER(14, 30),
-    VERSTECK(15, 10),
-    WALL(16, 20);
+    HAUPTGEBAEUDE(0, 30),
+    KASERNE(1, 25),
+    STALL(2, 20),
+    WERKSTATT(3, 15),
+    ADELSHOF(4, 1),
+    SCHMIEDE(5, 20),
+    VERSAMMLUNGSPLATZ(6, 1),
+    STATUE(7, 1),
+    MARKTPLATZ(8, 25),
+    HOLZFAELLER(9, 30),
+    LEHMGRUBE(10, 30),
+    EISENMINE(11, 30),
+    BAUERNHOF(12, 30),
+    SPEICHER(13, 30),
+    VERSTECK(14, 10),
+    WALL(15, 20);
 
     private final int id;
     private final int maxStufe;
