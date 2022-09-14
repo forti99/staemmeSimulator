1041b3340bd6fdc6afb4a4ada22042fc0ca12b90
Michael Fortenbacher <forti99@web.de>
2022-09-14 17:53:14 +0200

    Anpassung an die neue Ausgabemethode der Simulator-Klasse

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int anzahlVersuche = 1000;
         boolean belohnungAktiv = true;
 
-        SimErgebnis simErgebnis = simulator.nachStufenAusbauen(ausbaustart, ausbauziel, speicher, anzahlVersuche);
-        System.out.println(simErgebnis);
+        simulator.nachStufenAusbauen(ausbaustart, ausbauziel, speicher, anzahlVersuche);
+        simulator.ergebnisseDarstellen();
     }
 }
