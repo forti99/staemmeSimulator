7d9aacbed802a66f7914edde79e43fa5591e4ffb
Michael Fortenbacher <forti99@web.de>
2022-09-15 17:15:49 +0200

    Einstellungen-Klasse ist nicht mehr statisch

--- src/berechnungen/Einstellungen.java
+++ src/berechnungen/Einstellungen.java
@@ -4,13 +4,19 @@ import gebaeude.GebaeudeDaten;
 import gebaeude.GebaeudeTypen;
 
 public class Einstellungen {
-    public static final int weltengeschwindigkeit = 2;
-    public static final int minengeschwindigkeit = 2;
-    public static final int[] produktionsraten = {10, 30, 35, 41, 47, 55, 64, 74, 86, 100, 117, 136, 158, 184, 214, 249, 289, 337, 391, 455, 530, 616, 717, 833, 969, 1127, 1311, 1525, 1774, 2063, 2400};
-    public static final boolean belohnungenAktiv = true;
+    private int weltengeschwindigkeit;
+    private int minengeschwindigkeit;
+    private boolean belohnungenAktiv;
+    private int[] produktionsraten = {10, 30, 35, 41, 47, 55, 64, 74, 86, 100, 117, 136, 158, 184, 214, 249, 289, 337, 391, 455, 530, 616, 717, 833, 969, 1127, 1311, 1525, 1774, 2063, 2400};
+    
+    public Einstellungen (int weltengeschwindigkeit, int minengeschwindigkeit, boolean belohnungenAktiv) {
+        this.weltengeschwindigkeit = weltengeschwindigkeit;
+        this.minengeschwindigkeit = minengeschwindigkeit;
+        this.belohnungenAktiv = belohnungenAktiv;
+    }
     
     //Berechnet die Belohnungen die fuer das erstmalige Bauen einer Gebaeudestufe erhalten werden (10% der Baukosten aber mindestens 150 Holz, 150 Lehm und 100 Eisen und maximal 2000 pro Rohstoff)
-    public static Rohstoffe getBelohnung(GebaeudeTypen gebaeudeTyp, int stufe) {
+    public Rohstoffe getBelohnung(GebaeudeTypen gebaeudeTyp, int stufe) {
         Rohstoffe baukosten = new GebaeudeDaten().getBaukosten(gebaeudeTyp, stufe);
         Rohstoffe belohnung = new Rohstoffe();
 
@@ -19,5 +25,13 @@ public class Einstellungen {
         belohnung.setEisen(Math.min(2000, Math.max(100, (int) (baukosten.getEisen() * 0.1))));
 
         return belohnung;
+    }
+    
+    public int getProduktionsrate(int gebaeudeStufe){
+        return produktionsraten[gebaeudeStufe] * weltengeschwindigkeit * minengeschwindigkeit;   
+    }
+    
+    public isBelohnungAktiv() {
+        return belohnungAktiv;
     }
 }
