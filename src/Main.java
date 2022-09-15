da299d59df7fbe2e47ab8365512cb51ca7b8492c
Michael Fortenbacher <forti99@web.de>
2022-09-15 17:19:59 +0200

    Anpassung an die Veraenderung der Einstellungen-Klasse

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
+        Einstellungen einstellungen = new Einstellungen(2, 2, true);
+        
         Speicher speicher = new Speicher(1, 500, 500, 400);
         int[] ausbaustart = new int[]{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, speicher.getStufe(), 0, 0};
         int[] ausbauziel = new int[]{22, 25, 20, 15, 1, 20, 1, 1, 17, 30, 30, 30, 30, 30, 0, 20};
-
+        
         int anzahlVersuche = 1000;
-        boolean belohnungAktiv = true;
-
-        simulator.nachStufenAusbauen(ausbaustart, ausbauziel, speicher, anzahlVersuche);
+        
+        simulator.nachStufenAusbauen(ausbaustart, ausbauziel, speicher, anzahlVersuche, einstellungen);
         simulator.ergebnisseDarstellen();
     }
 }
